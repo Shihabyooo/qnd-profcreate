a6223eb1e609d9c2fe54a069e62aca13c47c4a5a
Shihabyooo <shihabnoor@hotmail.com>
2020-05-08 17:49:58 +0200

    Added file list display and selection, added options to add and select DEMs. Modified MainWindow to be at fixed position and size.

--- qnd-profcreate/MainWindow.cpp
+++ qnd-profcreate/MainWindow.cpp
@@ -1,19 +1,37 @@
 #include "MainWindow.h"
 
 
+char geometryFilePath[MAX_PATH] = "Enter path to geometry directory here or use the directory browser.";
+char demFilePath[MAX_PATH] = "Enter path to DEM directory here or use the directory browser.";
 std::unique_ptr<bool> selectedGeometry;
+std::unique_ptr<bool> selectedDEM;
 std::vector<std::string> geometryNames;
+std::vector<std::string> demNames;
 bool defaulSelectionState = true;
 
-void DrawGeometryList()
-{
-	//std::cout 
+void DrawFileList(char * filePath, std::vector<std::string> * fileNames, std::unique_ptr<bool> * selectionStates, DataType dataType)
+{	
+	ImGui::Separator();
+	ImGui::Text("Available Geometries\n");
+	ImGui::SameLine();
+	if (ImGui::Button("Update List", ImVec2(50, 20)))
+		UpdateFileList(filePath, fileNames, selectionStates, dataType);
+
+	ImGui::Spacing();
+	for (int i = 0; i < fileNames->size(); i++)
+	{		
+		bool state = selectionStates->get()[i]; //feeding selectedGeometry.get()[i] directly to ImGui::Selectable() prevents it from switching selection state.
+		ImGui::Selectable(ExtractFileName((*fileNames)[i]).c_str(), &state);
+		selectionStates->get()[i] = state;
+	}
+
+	ImGui::Separator();
 }
 
 void DrawMainWindow()
 {
-	ImGuiWindowFlags windowFlags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoCollapse;
-	ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_FirstUseEver);
+	ImGuiWindowFlags windowFlags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+	ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_FirstUseEver);
 	ImGui::SetNextWindowSize(ImVec2(500, 500), ImGuiCond_FirstUseEver);
 
 	ImGui::Begin("MainWindow", NULL, windowFlags);
@@ -32,23 +50,32 @@ void DrawMainWindow()
 	}
 
 	ImGui::Text("QnD Profile Creator v.0.x.x");
+	ImGui::Spacing();
+
 	ImGui::Separator();
-
 	ImGui::Text("Geometry Sources");
+	ImGui::InputText("Geometry File Path", geometryFilePath, IM_ARRAYSIZE(geometryFilePath));
 
-	static char filePath[MAX_PATH] = "test";
-	ImGui::InputText("File Path", filePath, IM_ARRAYSIZE(filePath));
-
-	if (ImGui::Button("Browse for directory"))
-	{
-		OpenFileBrowser(filePath, &geometryNames, selectedGeometry.get(), DataType::geometry);
-	}
-
+	if (ImGui::Button("Browse for geometry directory"))
+		OpenFileBrowser(geometryFilePath, &geometryNames, &selectedGeometry, DataType::geometry);
+	
 	DrawFileBrowser();
+	DrawFileList(geometryFilePath, &geometryNames, &selectedGeometry, DataType::geometry);
 
-	DrawGeometryList();
+	ImGui::Separator();
+	ImGui::Text("DEM");
+	ImGui::InputText("DEM File Path", demFilePath, IM_ARRAYSIZE(demFilePath));
 
+	if (ImGui::Button("Browse for DEM directory"))
+		OpenFileBrowser(demFilePath, &demNames, &selectedDEM, DataType::dem);
+	////DrawFileBrowser(); //The call is already made above...
+	DrawFileList(demFilePath, &demNames, &selectedDEM, DataType::dem);
+
+	ImGui::Spacing();
+	ImGui::Spacing();
+
+	
+	ImGui::Button("Process!", ImVec2(100, 50));
 
 	ImGui::End();
-
 }
